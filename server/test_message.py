2ba8a3a546fcd6de09ee9e1ce753cda9578fea10
Bridget McCarthy <braedyn.mccarthy@unsw.edu.au>
2019-09-29 23:53:32 +1000

    test_message.py stubs (testing pending)

---
+++ server/test_message.py
@@ -0,0 +1,27 @@
+# test_message.py
+# Authored by Bridget McCarthy 29/09/19
+
+def test_message_sendlater():
+    pass
+    
+def test_message_send():
+    pass
+    
+def test_message_remove():
+    pass
+    
+def test_message_edit():
+    pass
+    
+def test_message_react():
+    pass
+    
+def test_message_unreact():
+    pass
+    
+def test_message_pin():
+    pass
+    
+def test_message_unpin():
+    pass
+    
