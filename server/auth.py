a1e214d8bfa2bd818c5ccef550572ae5e39f3d42
Sabine Lim <thesabinelim@gmail.com>
2019-10-01 20:35:51 +1000

    Wrote basic auth tests

--- server/auth.py
+++ server/auth.py
@@ -2,15 +2,34 @@
 # Written by Sabine Lim z5242579
 # 01/10/19
 
-# Given a registered user email and password, returns dictionary containing u_id
-# and login token. Raises ValueError exception email entered is not valid/does
+# Given registered user email and password, return dictionary containing u_id
+# and auth token. Raise ValueError exception if email entered is not valid/does
 # not belong to a user, or password is incorrect.
-def auth_login(email, password) {
-    return {'u_id': 1234, 'token': '56789'}
-}
+def auth_login(email, password):
+    if email == 'bademail':
+        raise ValueError
+    if email == 'user@example.com' and password == 'validpassword':
+        return {'u_id': 1234, 'token': '56789'}
+    raise ValueError
 
 # Given an active token, invalidates the taken to log the user out. Given a
 # non-valid token, does nothing. Returns empty dictionary.
-def auth_logout(token) {
+def auth_logout(token):
     return {}
-}
+
+# Given first and last name, email address and password, create new user account
+# and return auth token. Raise ValueError exception if email entered is not
+# valid/already in use, password is not valid, name_first > 50 characters or
+# name_last > 50 characters
+def auth_register(email, password, name_first, name_last):
+    if email == 'bademail':
+        raise ValueError
+    if password == 'pwd':
+        raise ValueError
+    if name_first == '123456789012345678901234567890123456789012345678901':
+        raise ValueError
+    if name_last == '123456789012345678901234567890123456789012345678901':
+        raise ValueError
+    if email == 'user@example.com' and password == 'validpassword':
+        return {'u_id': 1234, 'token': '56789'}
+    raise ValueError
--- server/test_auth.py
+++ server/test_auth.py
@@ -2,8 +2,69 @@
 # Written by Sabine Lim z5242579
 # 29/09/19
 
+import pytest
+
 from auth import *
 
-def test_auth_register_login_simple():
-    response = auth_login("user@example.com", "123456")
-    assert response and 'u_id' response and 'token' in response
+#######################
+# auth_register Tests #
+#######################
+
+def test_auth_register_simple():
+    regDict = auth_register('user@example.com', 'validpassword', 'Test', 'User')
+    assert regDict and 'u_id' in regDict and 'token' in regDict
+
+def test_auth_register_bademail():
+    with pytest.raises(ValueError):
+        auth_register('bademail', 'validpassword', 'Test', 'User')
+
+def test_auth_register_badpwd():
+    with pytest.raises(ValueError):
+        auth_register('user@example.com', 'pwd', 'Test', 'User')
+
+def test_auth_register_badnames():
+    with pytest.raises(ValueError):
+        auth_register('user@example.com', 'validpassword',
+            '123456789012345678901234567890123456789012345678901', 'User')
+    with pytest.raises(ValueError):
+        auth_register('user@example.com', 'validpassword', 'Test',
+            '123456789012345678901234567890123456789012345678901')
+
+def test_auth_register_emailtaken():
+    # SETUP BEGIN
+    auth_register('user@example.com', 'validpassword', 'Test', 'User')
+    # SETUP END
+
+    with pytest.raises(ValueError):
+        auth_register('user@example.com', 'validpassword', 'Test', 'User')
+    with pytest.raises(ValueError):
+        auth_register('user@example.com', 'diffpassword', 'Test', 'User')
+
+####################
+# auth_login Tests #
+####################
+
+def test_auth_login_simple():
+    # SETUP BEGIN
+    regDict = auth_register('user@example.com', 'validpassword', 'Test', 'User')
+    # SETUP END
+
+    loginDict = auth_login('user@example.com', 'validpassword')
+    assert loginDict and 'u_id' in loginDict and 'token' in loginDict
+    assert loginDict['u_id'] == regDict['u_id']
+
+def test_auth_login_bademail():
+    with pytest.raises(ValueError):
+        auth_login('bademail', 'pwd')
+
+def test_auth_login_notreg():
+    with pytest.raises(ValueError):
+        auth_login('idontexist@example.com', 'validpassword')
+
+def test_auth_login_wrongpwd():
+    # SETUP BEGIN
+    regDict = auth_register('user@example.com', 'validpassword', 'Test', 'User')
+    # SETUP END
+
+    with pytest.raises(ValueError):
+        auth_login('user@example.com', 'wrongpassword')
