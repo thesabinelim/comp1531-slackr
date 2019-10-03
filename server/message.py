30a0a471a2675f88410b753513f3fdc7e40d8763
sharkyh20 <dooger1@live.com.au>
2019-10-03 10:49:56 +1000

    Implemented placeholder function stubs for message.py, ready to be used for writing tests.

--- server/message.py
+++ server/message.py
@@ -2,20 +2,25 @@
 # Written by Bridget McCarthy z5255505
 # 1/10/19
 
+import datetime
 # Send a message from authorised_user to the channel specified by channel_id.
 # automatically at a specified time in the future. 
 # Raises ValueError exception when channel_id doesn't exist, messige is more
 # than 1000 characters, or time_sent is a past time.
 # No return value.
 def message_sendlater(token, channel_id, message, time_sent):
-    pass
+    if len(message) > 1000:
+        raise ValueError
+    if time_sent < datetime.datetime.now():
+        raise ValueError
+    if channel_id < 0:
+        raise ValueError
 
 # Send a message from authorised_user to the channel specified by channel_id.
 # Raises ValueError exception when the message is more than 1000 characters.
 # No return value.
 def message_send(token, channel_id, message):
-    # Can't exactly test token stuff right now
-    if message.length() > 1000:
+    if len(message) > 1000:
         raise ValueError
 
     # An assumption I'm making is that the channel_id has to be valid, like in
@@ -25,30 +30,89 @@ def message_send(token, channel_id, message):
 
 # Given a message_id for a message, this message is removed from the channel.
 # Raises ValueError when the message_id no longer exists.
+# Raises AccessError when message_id not sent by authorised user, AND message_id
+# was not sent by an owner of the channel AND message_id was not sent by an admin
+# or owner of the slack.
 # No return value.
 def message_remove(token, message_id):
-    pass
+    if channel_id < 0:
+        raise ValueError
+    if message_id == '420':
+        raise AccessError
 
-# Given a message, update it's text with new text. No return value.
+# Given a message, update it's text with new text.
+# Raises ValueError when message_id not sent by authorised user, AND message_id
+# was not sent by an owner of the channel AND message_id was not sent by an admin
+# or owner of the slack.
+# No return value.
 def message_edit(token, message_id, message):
-    pass
+    if message_id == '420':
+        raise ValueError
+
 
 # Given a message within a channel the authorised user is part of, add a "react"
-# to that particular message. No return value.
+# to that particular message. 
+# Raises ValueError when message_id is not a valid message within a channel 
+# authorised user has joined.
+# Raises ValueError when react_id is not valid.
+# Raises ValueError when message for message_id already has a reaction with
+# the react_id.
+# No return value.
 def message_react(token, message_id, react_id):
-    pass
+    if message_id == '420':
+        raise ValueError
+    if react_id < 0:
+        raise ValueError
+    if react_id == '10' and message_id == '20':
+        raise ValueError
 
 # Given a message within a channel the authorised user is part of, remove a 
-# "react" to that particular message. No return value.
+# "react" to that particular message.
+# Raises ValueError when message_id is not a valid message within a channel 
+# authorised user has joined.
+# Raises ValueError when react_id is not valid.
+# Raises ValueError when message for message_id doesn't have a reaction with
+# the react_id.
+# No return value.
 def message_unreact(token, message_id, react_id):
-    pass
+    if message_id == '420':
+        raise ValueError
+    if react_id < 0:
+        raise ValueError
+    if react_id == '10' and message_id != '20':
+        raise ValueError
 
 # Given a message within a channel, mark it as "pinned" to be given special 
-# display treatment by the frontend. No return value.
+# display treatment by the frontend.
+# Raises ValueError when message_id is not a valid message.
+# Raises ValueError when authorised user is not an admin.
+# Raises ValueError when message_id is already pinned.
+# Raises AccessError when authorised user is not a member of channel for
+# the message.
+# No return value.
 def message_pin(token, message_id):
-    pass
+    if message_id == '420':
+        raise ValueError
+    if token == '1234567':
+        raise ValueError
+    if message_id == '20':
+        raise ValueError
+    if token == '7849321':
+        raise AccessError
 
-# Given a message within a channel, remove it's mark as unpinned. No return 
-# value.
+# Given a message within a channel, remove it's mark as unpinned.
+# Raises ValueError when message_id is not a valid message.
+# Raises ValueError when authorised user is not an admin.
+# Raises ValueError when message_id isn't pinned.
+# Raises AccessError when authorised user is not a member of channel for
+# the message.
+# No return value.
 def message_unpin(token, message_id):
-    pass
+    if message_id == '420':
+        raise ValueError
+    if token == '1234567':
+        raise ValueError
+    if message_id != '20':
+        raise ValueError
+    if token == '7849321':
+        raise AccessError
