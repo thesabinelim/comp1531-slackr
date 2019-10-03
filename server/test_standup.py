843a3e91a6b8564dd0925b513da19133add06172
Jake <tylerjake7@gmail.com>
2019-10-03 11:24:27 +1000

    added more tests should have coverage over the given exceptions

--- server/test_standup.py
+++ server/test_standup.py
@@ -11,15 +11,11 @@ from error import *
 from channel import *
 from auth import *
 
-
 #######################
 # standup_start Tests #
 #######################
 
-# the following tests involve token input and may
-# need to be adjusted later
-
-# trying to standup_start a nonexistent channel
+# trying to standup_start a nonexistent channel should return a ValueError
 def test_standup_start_invalidchannel():
     auth_register('user@example.com', 'validpassword', 'Test', 'User')
     token = authRegisterDict['token']
@@ -28,7 +24,7 @@ def test_standup_start_invalidchannel():
         standup_start(token, 'Channel Name')
 
 # creating then leaving a channel then testing for being able
-# to standup_start that channel.
+# to standup_start that channel. This should return an AccessError.
 def test_standup_start_notmember():
     auth_register('user@example.com', 'validpassword', 'Test', 'User')
     token = authRegisterDict['token']
@@ -46,7 +42,8 @@ def test_standup_start_notmember():
 #  standup_send Tests #
 #######################
 
-# a standup is running however the user has left so can't add to queue
+# a standup is running however the user has left the channel so doesn't
+# have permission to add to queue should return a AccessError
 def test_standup_send_notmember():
     auth_register('user@example.com', 'validpassword', 'Test', 'User')
     token = authRegisterDict['token']
@@ -60,8 +57,41 @@ def test_standup_send_notmember():
     with pytest.raises(AccessError):
         standup_send(token, channelId, 'This should not add')
 
+# tests that when the channel doesn't exist that a ValueError returns
+def test_standup_send_nochannel():
+    auth_register('user@example.com', 'validpassword', 'Test', 'User')
+    token = authRegisterDict['token']
+    
+    with pytest.raises(ValueError):
+        standup_send(token, channelId, 'This should not add')
 
+# tests that when the standup_time is 0 a ValueError is returned
+def test_standup_send_time():
+    auth_register('user@example.com', 'validpassword', 'Test', 'User')
+    token = authRegisterDict['token']
+    
+    channelsCreateDict = channels_create(token, 'channel name', public)
+    channelId = channelsCreateDict['channelId']
+    
+    time = standup_start(token, channelId)
+    time = 0
+    
+    with pytest.raises(ValueError):
+        standup_send(token, channelId, 'This should not add')
 
+# tests that when the length of the string is over 1000 characters a
+# ValueError is returned
+def test_standup_send_length():
+    auth_register('user@example.com', 'validpassword', 'Test', 'User')
+    token = authRegisterDict['token']
+    
+    channelsCreateDict = channels_create(token, 'channel name', public)
+    channelId = channelsCreateDict['channelId']
+    
+    time = standup_start(token, channelId)
+    
+    with pytest.raises(ValueError):
+        standup_send(token, channelId, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
 
 
 
