42c7d810219613a2eb22cb10d609bb59870d9ad9
Sabine Lim <thesabinelim@gmail.com>
2019-10-05 01:07:40 +1000

    Added AccessError to channel.py

--- server/channel.py
+++ server/channel.py
@@ -2,6 +2,8 @@
 # Written by Sabine Lim z5242579
 # 01/10/19
 
+from error import *
+
 # Invite user with u_id to channel with channel_id. Returns {} if successful.
 # Raises ValueError exception if channel_id is invalid/user is not in channel or
 # if u_id is invalid.
