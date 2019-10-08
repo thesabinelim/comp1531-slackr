375b64b4d0012414996422ce087102cd32b5abb2
Hayden Smith <mail@chambalam.com>
2019-10-08 23:35:04 +1100

    Iteration 2 setup

---
+++ server.py
@@ -0,0 +1,22 @@
+"""Flask server"""
+from json import dumps
+from flask import Flask, request
+
+APP = Flask(__name__)
+
+@APP.route('/echo/get', methods=['GET'])
+def echo1():
+    """ Description of function """
+    return dumps({
+        'echo' : request.args.get('echo'),
+    })
+
+@APP.route('/echo/post', methods=['POST'])
+def echo2():
+    """ Description of function """
+    return dumps({
+        'echo' : request.form.get('echo'),
+    })
+
+if __name__ == '__main__':
+    APP.run()
