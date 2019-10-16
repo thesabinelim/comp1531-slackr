93dd2a6b0aaeb291c1eb15105d1b0125c7fb9d67
Sabine Lim <thesabinelim@gmail.com>
2019-10-17 00:42:41 +1100

    Added random string function to utils.py

--- utils.py
+++ utils.py
@@ -3,8 +3,17 @@
 # 16/10/19
 
 import re
+import random
+import string
 
-# Return True if email is valid, 0 otherwise
+# Return True if email is valid, 0 otherwise.
 def is_valid_email(email):
     email_re = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
     return True if re.search(email_re, email) else False
+
+# Return randomly generated string with given length. String can contain
+# alphanumeric characters and special characters.
+def random_string(length):
+    characters = string.ascii_letters + string.digits + string.punctuation \
+        + string.printable
+    return ''.join(random.choice(characters) for _ in range(length))
