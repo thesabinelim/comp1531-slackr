89311bc1dc11829ba38a5805aa72ff453715faa1
sharkyh20 <dooger1@live.com.au>
2019-10-20 13:41:22 +1100

    Missed server.py from last commit

--- server.py
+++ server.py
@@ -120,9 +120,6 @@ def req_auth_register():
     name_first = request.form.get('name_first')
     name_last = request.form.get('name_last')
 
-    token = auth_register(email, password, name_first, name_last)
-    u_id = db_get_user_by_email(email).get_user_id()
-
     return dumps(auth_register(email, password, name_first, name_last))
 
 
