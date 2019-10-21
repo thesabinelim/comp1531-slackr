4bc305e443db92024410fcdeb70b578ad751b063
Rob Everest <rob.everest@gmail.com>
2019-10-21 22:55:01 +1100

    Generated prebundle for channel creation bug

--- frontend/prebundle/service-worker.js
+++ frontend/prebundle/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9da77a4685b589298c523ee9924f84fa.js"
+  "/precache-manifest.53e7ec5d57012d19c5f75d366140f560.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
