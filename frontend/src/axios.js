8854b46daf53cf43b24251fae3913e16f9b0e288
hugh <hughetchan@gmail.com>
2019-10-23 00:43:10 +1100

    Fixing axios to send DELETE and PUT data as Form Data

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -4,7 +4,9 @@ import qs from 'qs';
 import { url } from './utils/constants';
 
 axios.defaults.baseURL = url;
+axios.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.delete['Content-Type'] = 'application/x-www-form-urlencoded';
 
 axios.interceptors.request.use((request) => {
     if (request.method === 'put' || request.method === 'post' || request.method === 'delete') {
