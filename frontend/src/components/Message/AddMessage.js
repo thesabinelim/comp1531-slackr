cadd1022b1bb5bd1080e8cb7d38e2f08aea46bf1
Hayden Smith <mail@chambalam.com>
2019-10-28 15:52:56 +1100

    Early draft for iteration 3

--- frontend/src/components/Message/AddMessage.js
+++ frontend/src/components/Message/AddMessage.js
@@ -54,7 +54,7 @@ function AddMessage({ channel_id = '', onAdd = () => {} }) {
         token,
         channel_id,
         message,
-        time_sent: currentTimer.getTime(),
+        time_sent: (currentTimer.getTime() / 1000),
       })
         .then(({ data }) => {
           console.log(data);
