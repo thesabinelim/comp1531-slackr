98a914e1bb15559656c837f85fac47c822fb6bc8
Sabine Lim <thesabinelim@gmail.com>
2019-11-15 23:56:20 +1100

    Split search into two functions

--- backend/search.py
+++ backend/search.py
@@ -4,25 +4,28 @@
 
 from .auth import validate_token
 
-from .db import (
-    db_get_user_by_u_id
-)
-from .channel import (
-    channel_messages
-)
+from .db import db_get_user_by_u_id
+from .channel import channel_messages
+
+# Given a query string, return a collection of messages in a specific channel that match the query
+def search_channel(token, channel, query_str):
+    results = []
+    start = 0
+    while start != -1: 
+        messages = channel_messages(token, channel.get_channel_id(), start)
+        for message in messages['messages']:
+            if query_str.lower() in message['message'].lower():
+                results.append(message)
+        start = messages['end']
+    return results
+
 # Given a query string, return a collection of messages that match the query
 def search(token, query_str):
     user = validate_token(token)
+
     channels = user.get_channels()
 
-    search_messages = []
+    results = []
     for channel in channels:
-        start = 0
-        match_message = []
-        while start != -1: 
-            match_message = channel_messages(token, channel.get_channel_id(), start)
-            for channel_message in match_message['messages']:
-                if query_str.lower() in channel_message['message'].lower():
-                    search_messages.append(channel_message)
-            start = match_message['end']
-    return {'messages': search_messages}
+        results.extend(search_channel(token, channel, query_str))
+    return {'messages': results}
