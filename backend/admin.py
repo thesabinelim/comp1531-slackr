5449fcf80970671436066c89c4c349e0c188125c
Jake <tylerjake7@gmail.com>
2019-11-16 23:24:33 +1100

    minor adjustments

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,5 @@
 # COMP1531 Project admin
 # Written by Sabine Lim z5242579
-# Refactored by Jake Tyler z5208823
 # 22/10/19
 
 from .db import Role, User, db_get_user_by_u_id
@@ -13,52 +12,43 @@ from .error import ValueError, AccessError
 # Raise ValueError for invalid u_id or permission_id.
 # Raise AccessError when user is not admin or owner.
 
-############################## User Permissions Change ########################################
+############################## Channel Setup ########################################
 
-# authorises users, checks errors then changes and returns.
+# main function, setup for readability
 def admin_userpermission_change(token, target_id, permission_id):
+
+    # sets up user and the target.
+    authorised_user, target = admin_userpermission_change_setup(token, target_id)
     
-    # authorises the user
-    authorised_user, target = admin_setup(token, target_id)
-    
-    # checks for errors
-    admin_errors(target, permission_id, authorised_user)
-    
-    # reassigns permission id
+    # error checks
+    admin_userpermission_change_error(authorised_user, target, permission_id)
+
+    # sets the required permission_id
     target.set_slackr_role(Role(permission_id))
-    
+
     return {}
 
-# validates both users then returns the validated user and target
-def admin_setup(token, target_id):
+def admin_userpermission_change_setup(token, target_id):
+
+    authorised_user = validate_token(token)
     
-    u_id = validate_token(token)
-    authorised_user = db_get_user_by_u_id(u_id)
     target = db_get_user_by_u_id(target_id)
-    
+
     return authorised_user, target
 
-# checks all errors
-def admin_errors(target, permission_id, authorised_user):
-    
-    # if the target is non-existent
-    if target is None:
-        raise ValueError(description = "User with u_id does not exist!")
-    
-    # if the permission id isnt an actual id
-    if permission_id not in [perm.value for perm in Role]:
-        raise ValueError(description = "Invalid permission_id!")
+# error list
+def admin_userpermission_change_error(authorised_user, target, permission_id):
 
-    # if the user trying to change the permission does not have sufficient permissions to do so
+    if permission_id not in [perm.value for perm in Role]:
+        raise ValueError(description="Invalid permission_id!")
+    
     if authorised_user.get_slackr_role() != Role.owner \
         and authorised_user.get_slackr_role() != Role.admin:
-        raise AccessError(description = "Logged in user is not admin or owner!")
+        raise AccessError(description="Logged in user is not admin or owner!")
     
-    # the target user has higher permissions
     if target.get_slackr_role().value < authorised_user.get_slackr_role().value:
-        raise AccessError(description = "Cannot modify permissions of a user with higher permissions")
+        raise AccessError(description="Cannot modify permissions of a user with higher permissions")
     
-    # changing the targets permissions to higher then the users.
     if permission_id < authorised_user.get_slackr_role().value:
-        raise AccessError(description = "Cannot modify permissions to be higher than authorised user's own permissions")
+        raise AccessError(description="Cannot modify permissions to be higher than authorised user's own permissions")
 
